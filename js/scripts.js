1352d5f643a668946d7acef79d7e0a97a5d9b3ba
lexa <keitmark@mail.ru>
2018-01-17 17:41:06 +0300

    save

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
+
+    $('.sl-2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
     
-    
-     
+
 });
-
-
