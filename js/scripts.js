bf7ac03c2656d42e75d1ab350a0f3040f078ce0f
lexa <keitmark@mail.ru>
2018-01-18 18:01:51 +0300

    save

--- js/scripts.js
+++ js/scripts.js
@@ -43,16 +43,20 @@ $(document).ready(function () {
         if($(this).index()){
             $('.body-sec-time-line .left-side').removeClass('active for-w-1');
             $('.body-sec-time-line .right-side').addClass('active for-w-1');
+            $('.spr-t').removeClass('active');
+            $('.sum-t').addClass('active');
         }
         else{
             $('.body-sec-time-line .right-side').removeClass('active for-w-1');
             $('.body-sec-time-line .left-side').addClass('active for-w-1');
+            $('.sum-t').removeClass('active');
+            $('.spr-t').addClass('active');
         }
         
     });
     
     
-       $('.new-slider-3').slick({
+    $('.new-slider-3').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: true,
@@ -67,5 +71,24 @@ $(document).ready(function () {
         ]
     }); 
     
+    
+    
+    $('.uniq-class').click(function(){
+        $(".uniq-class").removeClass('active');
+        $(this).addClass('active');
+        
+        if($(this).index()){
+            $('.sl-2').slick('slickGoTo',$(this).index()+1);
+        }
+        else{
+            $('.sl-2').slick('slickGoTo',$(this).index());
+        }
+        
+    });
+    
+    $('.sl-2').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $(".uniq-class").removeClass('active');
+        $(".uniq-class").eq(nextSlide).addClass('active')
+    });
     
 });
